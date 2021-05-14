facebook/mysql-5.6

Add RocksDB parameter fs_uri for specifying custom file systems
Summary:

Add support for specifying custom RocksDB file system implementations
through the parameter rocksdb_fs_uri.

Examples:
    rocksdb_fs_uri=zenfs://dev:nvme0n1
    rocksdb_fs_uri=posix://

Custom file systems may be built as plugins in rocksdb and can be
created through a factory function that will be looked up by the
object registry. The posix file system is built in.

storage/rocksdb/ha_rocksdb.cc
@@ -686,6 +686,7 @@ static long long rocksdb_compaction_sequential_deletes_window = 0l;
 static long long rocksdb_compaction_sequential_deletes_file_size = 0l;
 static uint32_t rocksdb_validate_tables = 1;
 static char *rocksdb_datadir;
+static char *rocksdb_fs_uri;
 static uint32_t rocksdb_max_bottom_pri_background_compactions=0;
 static uint32_t rocksdb_table_stats_sampling_pct;
 static uint32_t rocksdb_table_stats_recalc_threshold_pct = 10;
@@ -894,6 +895,12 @@ static std::unique_ptr<rocksdb::DBOptions> rdb_init_rocksdb_db_options(void) {
   o->two_write_queues = true;
   o->manual_wal_flush = true;
   return o;
+}
+
+static ROCKSDB_NAMESPACE::Env *GetCompositeEnv(std::shared_ptr<ROCKSDB_NAMESPACE::FileSystem> fs)
+{
+  static std::shared_ptr<ROCKSDB_NAMESPACE::Env> composite_env = ROCKSDB_NAMESPACE::NewCompositeEnv(fs);
+  return composite_env.get();
 }
 
 /* DBOptions contains Statistics and needs to be destructed last */
@@ -2147,6 +2154,11 @@ static MYSQL_SYSVAR_STR(datadir, rocksdb_datadir,
                         "RocksDB data directory", nullptr, nullptr,
                         "./.rocksdb");
 
+static MYSQL_SYSVAR_STR(fs_uri, rocksdb_fs_uri,
+                        PLUGIN_VAR_OPCMDARG | PLUGIN_VAR_READONLY,
+                        "Custom filesystem URI", nullptr,
+                        nullptr, nullptr);
+
 static MYSQL_SYSVAR_UINT(
     table_stats_sampling_pct, rocksdb_table_stats_sampling_pct,
     PLUGIN_VAR_RQCMDARG,
@@ -2483,6 +2495,7 @@ static struct st_mysql_sys_var *rocksdb_system_variables[] = {
     MYSQL_SYSVAR(print_snapshot_conflict_queries),
 
     MYSQL_SYSVAR(datadir),
+    MYSQL_SYSVAR(fs_uri),
     MYSQL_SYSVAR(create_checkpoint),
 
     MYSQL_SYSVAR(checksums_pct),
@@ -5615,6 +5628,21 @@ static int rocksdb_init_func(void *const p) {
         "RocksDB: Can't enable both use_direct_reads "
         "and allow_mmap_reads\n");
     DBUG_RETURN(HA_EXIT_FAILURE);
+  }
+
+  if (rocksdb_fs_uri) {
+          std::shared_ptr<ROCKSDB_NAMESPACE::FileSystem> fs;
+          sql_print_information(
+              "RocksDB: Setting up custom file system, URI: %s\n",
+              rocksdb_fs_uri);
+          s = ROCKSDB_NAMESPACE::FileSystem::Load(rocksdb_fs_uri, &fs);
+          if (fs == nullptr) {
+                 sql_print_error(
+                    "RocksDB: Setting up custom file system failed: %s\n",
+                    s.ToString().c_str());
+                 DBUG_RETURN(HA_EXIT_FAILURE);
+          }
+          rocksdb_db_options->env = GetCompositeEnv(fs);
   }
 
   // Check whether the filesystem backing rocksdb_datadir allows O_DIRECT

percona-ysorokin/percona-server

(cherry picked from commit 57d25d227108146dbfc096ef9193c7b2486958dc)

storage/rocksdb/ha_rocksdb.cc
@@ -637,6 +637,7 @@ static uint32_t rocksdb_validate_tables = 1;
 #endif  // defined(ROCKSDB_INCLUDE_VALIDATE_TABLES) &&
         // ROCKSDB_INCLUDE_VALIDATE_TABLES
 static char *rocksdb_datadir = nullptr;
+static char *rocksdb_fs_uri;
 static uint32_t rocksdb_max_bottom_pri_background_compactions = 0;
 static uint32_t rocksdb_table_stats_sampling_pct =
     RDB_DEFAULT_TBL_STATS_SAMPLE_PCT;
@@ -817,6 +818,12 @@ rdb_init_rocksdb_tbl_options(void) {
       new rocksdb::BlockBasedTableOptions());
   o->block_size = RDB_DEFAULT_BLOCK_SIZE;
   return o;
+}
+
+static rocksdb::Env *GetCompositeEnv(std::shared_ptr<rocksdb::FileSystem> fs) {
+  static std::shared_ptr<rocksdb::Env> composite_env =
+      rocksdb::NewCompositeEnv(fs);
+  return composite_env.get();
 }
 
 /* DBOptions contains Statistics and needs to be destructed last */
@@ -2033,6 +2040,10 @@ static MYSQL_SYSVAR_STR(datadir, rocksdb_datadir,
                         "RocksDB data directory", nullptr, nullptr,
                         "./.rocksdb");
 
+static MYSQL_SYSVAR_STR(fs_uri, rocksdb_fs_uri,
+                        PLUGIN_VAR_OPCMDARG | PLUGIN_VAR_READONLY,
+                        "Custom filesystem URI", nullptr, nullptr, nullptr);
+
 static MYSQL_SYSVAR_UINT(
     table_stats_sampling_pct, rocksdb_table_stats_sampling_pct,
     PLUGIN_VAR_RQCMDARG,
@@ -2306,6 +2317,7 @@ static struct SYS_VAR *rocksdb_system_variables[] = {
     MYSQL_SYSVAR(print_snapshot_conflict_queries),
 
     MYSQL_SYSVAR(datadir),
+    MYSQL_SYSVAR(fs_uri),
     MYSQL_SYSVAR(create_checkpoint),
     MYSQL_SYSVAR(create_temporary_checkpoint),
     MYSQL_SYSVAR(disable_file_deletions),
@@ -5200,6 +5212,18 @@ static int rocksdb_init_internal(void *const p) {
         "Can't enable both use_direct_reads and allow_mmap_reads\n");
     deinit_logging_service_for_plugin(&reg_srv, &log_bi, &log_bs);
     DBUG_RETURN(HA_EXIT_FAILURE);
+  }
+
+  if (rocksdb_fs_uri) {
+    std::shared_ptr<rocksdb::FileSystem> fs;
+    s = rocksdb::FileSystem::Load(rocksdb_fs_uri, &fs);
+    if (fs == nullptr) {
+      LogPluginErrMsg(ERROR_LEVEL, 0, "Loading custom file system failed: %s\n",
+                      s.ToString().c_str());
+      deinit_logging_service_for_plugin(&reg_srv, &log_bi, &log_bs);
+      DBUG_RETURN(HA_EXIT_FAILURE);
+    }
+    rocksdb_db_options->env = GetCompositeEnv(fs);
   }
 
   // Check whether the filesystem backing rocksdb_datadir allows O_DIRECT