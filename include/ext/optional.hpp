e12678317f18569c65f72381c00d783430986724
Zsolt Parragi <zsolt.parragi@percona.com>
2023-06-07 12:00:12 +0200

    PS-8728 feature: Align Percona's Data Masking functionality with Oracle 8.0.33 (removed ext namespace)
    https://jira.percona.com/browse/PS-8728
    
    As both Oracle MySQL Server and Percona Server for MySQL now compile sources
    in c++17 mode, 'string_view' and 'optional' using declarations in the 'ext'
    namespace (aliasing corresponding class templates from the 'boost' library)
    are no longer needed. Instead we now use standard 'std::optional'
    from the '<optional>' header and 'std::string_view' from the '<string_view>'
    header.
    
    Some direct usages of 'boost::optional' also changed to 'std::optional'.

--- include/ext/optional.hpp
+++ include/ext/optional.hpp
@@ -17,14 +17,13 @@
 #ifndef EXT_OPTIONAL_HPP
 #define EXT_OPTIONAL_HPP
 
-#include <boost/optional.hpp>
+#include <optional>
 
 namespace ext {
 
-// TODO: the following 'using' directive can be changed to
-//   using std::optional;
-// once MySQL source code switches to c++17
-using boost::optional;
+// this using declaration left only for compatibility reasons
+// all new code should use std::string_view
+using std::optional;
 
 }  // namespace ext
 
